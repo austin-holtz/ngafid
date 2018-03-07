8874e6f1cffa1aab0a93d8d56b3cee232a8216b4
Sai Peri <sai.peri@ndus.edu>
2018-03-07 02:37:08 -0600

    Dynamically getting lat, long, height from database added

--- resources/views/turnToFinalDisplay.blade.php
+++ resources/views/turnToFinalDisplay.blade.php
@@ -1,36 +1,96 @@
-@extends ('NGAFID-master')
-
-@section('content')
-
-<html lang="en">
+<!DOCTYPE html>
+<html>
 <head>
-	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE-Edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, minimum-scale-1, user-scalable=no">
-	<title>Turn to Final</title>
-	<!--Cesium libraries -->
-	<script src="ThirdParty/Cesium/Cesium.js"></script>
-	<style>@import url(../../public/Cesium/ThirdParty/Cesium/Widgets/widgets.css);</style>
-	<link rel="stylesheet" href="../../public/Cesium/index.css">
+     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, minimum-scale=1, user-scalable=no">
+    <title>Test</title>
+       <script type="text/javascript" src="{{ asset('CesiumOld/Apps/Sandcastle/Sandcastle-header.js') }}"></script>
+       <script type="text/javascript" src="{{ asset('CesiumOld/ThirdParty/requirejs-2.1.9/require.js') }}"></script>
+
+        <script type="text/javascript">
+        require.config({
+            baseUrl: "{{ asset('CesiumOld/Source') }}",
+            waitSeconds: 60
+        });
+    </script>
+  
 </head>
-<body>
-	<div id="cesiumContainer"></div>
-	<script src="../../public/Cesium/Source/App.js"></script>
+<body class="sandcastle-loading" data-sandcastle-bucket="bucket-requirejs.html">
+    <style>
+        @import url("{{ asset('CesiumOld/Apps/Sandcastle/templates/bucket.css') }}");
+    </style>
+    <div id="cesiumContainer" class="fullSize"></div>
+    
+
+    <script id="cesium_sandcastle_script">
+        function startup(Cesium) 
+        {
+            'use strict';
+            //Sandcastle_Begin
+            /**
+				Requesting unique key in order to use bing map API
+            **/
+            Cesium.BingMapsApi.defaultKey = 'AoUP29Z-v0eqHOJaE4BaVhYJ1XuRZX04Oeiiw8if5KliJq7BbbJw9t0IrPe-Uix1';
+
+            /**
+				Set up viewer variable that will hold all visual additions to container
+            **/
+            var viewer = new Cesium.Viewer('cesiumContainer');
+
+            /**
+				Default location which is set to grand forks with default camera controls setup
+            **/
+            var gfLocation = new Cesium.Cartesian3.fromDegrees(-97.181238, 47.957674, 2631.0827);
+            var homeCameraView = {
+                destination: gfLocation,
+                orientation: {
+                    heading: 0.0,
+                    pitch: -Cesium.Math.PI_OVER_TWO,
+                    roll: 0.0
+                }
+            };
+
+            viewer.scene.camera.setView(homeCameraView);
+            viewer.homeButton.viewModel.command.beforeExecute.addEventListener(function (e) {
+    			e.cancel = true;
+    			viewer.scene.camera.flyTo(homeCameraView);
+
+    	<?php
+        	echo "var obj = $data;";
+    	?>
+
+    	var points = obj.split(',');
+    	//Delete last element due to it being a ','
+    	points.pop();
+    	//console.log(points); Debugging purposes
+
+    	var flightPath = viewer.entities.add({
+    	name: "Test flight",
+    	polyline: {
+        	<?php
+        	$newstr = chop($data,',');
+        	echo "positions: Cesium.Cartesian3.fromDegreesArray(points),"; 
+        	?>
+        	// positions: Cesium.Cartesian3.fromDegreesArrayHeights([]), //Includes heights for the lines as well
+        	width: 2,
+        	material: Cesium.Color.RED
+    	}
+	});
+	viewer.zoomTo(viewer.entities);
+
+	});
+
+	// Set up clock and timeline.
+	viewer.clock.shouldAnimate = true; // default
+    	        //Sandcastle_End
+        	    Sandcastle.finishedLoading();
+        	}
+        	if (typeof Cesium !== "undefined") {
+            	startup(Cesium);
+        	} else if (typeof require === "function") {
+            	require(["Cesium"], startup);
+        	}
+    </script>
+
+
 </body>
 </html>
-
-<!--
-<script>
-    <?php
-        echo "var obj = $data;";
-    ?>
-    // obj = JSON.parse(obj)
-    var testArr = obj.f381001.split(',');
-    testArr.forEach(function(element) {
-        console.log(element);
-    });
-    
-</script>
--->
-
-@endsection
